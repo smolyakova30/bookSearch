26987ba92fbfd715bd3218e3dc2e76d8261b6c45
Olga <o.smolyakova30@gmail.com>
2021-10-23 09:16:47 -0500

    updates server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+const startServer = async () => {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+ 
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
